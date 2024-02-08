5e67ea3d516fb41c20ab4e48fed493183003604b
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2024-02-08 23:57:08 +0530

    chore: update 1 file and create 4 files update .gitignore

---
+++ src/main/java/com/d4rkr0n1n/poc/PocApplication.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d4rkr0n1n/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
