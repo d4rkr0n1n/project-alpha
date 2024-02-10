1ededc39d88db1b4b91f59aad235c3ead447177c
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2024-02-10 15:18:04 +0530

    Add StudentController and StudentService classes

---
+++ src/main/java/com/d4rkr0n1n/poc/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.d4rkr0n1n.poc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.d4rkr0n1n.poc.model.Student;
+import com.d4rkr0n1n.poc.service.StudentService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class StudentController {
+    
+
+    @GetMapping("/test")
+    public Student test() {
+        return StudentService.testService();
+    }
+    
+}
